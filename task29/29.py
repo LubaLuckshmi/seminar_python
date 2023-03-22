49c0f7eb24b6eade7167842724b8b57805cd0ec7
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-03-22 18:10:02 +0300

    Решили задачу 29

---
+++ task29/29.py
@@ -0,0 +1,15 @@
+'''
+1.Создайте список из случайных чисел. Найдите номер его последнего 
+локального максимума (локальный максимум — это элемент, который 
+больше любого из своих соседей).
+'''
+import random
+
+some_list = [random.randint(1, 100) for _ in range(100)]
+print(some_list)
+
+for i in range(len(some_list) - 2, 0, -1):
+    if some_list[i - 1] < some_list[i] > some_list[i + 1]:
+        print(i)
+        print(some_list[i])
+        break
