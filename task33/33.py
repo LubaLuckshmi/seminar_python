56d3f4c06d2f79d838cb8d0d9a64512a2a5ffef8
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-03-24 13:25:02 +0300

    РЕшили задачу 33

--- task33/33.py
+++ task33/33.py
@@ -1,6 +1,25 @@
-
-
 # Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные.
 # Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
 
-# Напишите функцию, которая принимает одно число и проверяет, является ли оно простым
+def max_to_min(input_list: list):
+    min_number = input_list[0]
+    max_number = input_list[0]
+    for i in range(len(input_list)):
+        if input_list[i] > max_number:
+            max_number = input_list[i]
+        if input_list[i] < min_number:
+            min_number = input_list[i]
+    for i in range(len(input_list)):
+        if input_list[i] == max_number:
+            input_list[i] = min_number
+
+
+
+input_list = []
+list_len = int(input("Введите количество элементов "))
+for _ in range(list_len):
+    input_list.append(int(input("Введите число: ")))
+print(input_list)
+
+max_to_min(input_list)
+print(input_list)
